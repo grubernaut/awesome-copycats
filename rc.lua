0b42ff1186739fd0c184424413948482750da85e
Jake Champlin <jake.champlin@minted.com>
2015-01-18 03:00:18 -0500

    Remove MPD

--- rc.lua
+++ rc.lua
@@ -143,29 +143,6 @@ mailwidget = wibox.widget.background(lain.widgets.imap({
 }), "#313131")
 ]]
 
--- MPD
-mpdicon = wibox.widget.imagebox(beautiful.widget_music)
-mpdicon:buttons(awful.util.table.join(awful.button({ }, 1, function () awful.util.spawn_with_shell(musicplr) end)))
-mpdwidget = lain.widgets.mpd({
-    settings = function()
-        if mpd_now.state == "play" then
-            artist = " " .. mpd_now.artist .. " "
-            title  = mpd_now.title  .. " "
-            mpdicon:set_image(beautiful.widget_music_on)
-        elseif mpd_now.state == "pause" then
-            artist = " mpd "
-            title  = "paused "
-        else
-            artist = ""
-            title  = ""
-            mpdicon:set_image(beautiful.widget_music)
-        end
-
-        widget:set_markup(markup("#EA6F81", artist) .. title)
-    end
-})
-mpdwidgetbg = wibox.widget.background(mpdwidget, "#313131")
-
 -- MEM
 memicon = wibox.widget.imagebox(beautiful.widget_mem)
 memwidget = lain.widgets.mem({
